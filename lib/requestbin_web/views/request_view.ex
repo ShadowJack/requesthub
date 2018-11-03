94f3d99db3b14ae86959cdf914e01f3ade1387ef
shadowjack <kostyap@hotmail.ru>
2018-11-03 16:49:25 +0300

    Prettify a bit

--- lib/requestbin_web/views/request_view.ex
+++ lib/requestbin_web/views/request_view.ex
@@ -1,5 +1,6 @@
 defmodule RequestbinWeb.RequestView do
   use RequestbinWeb, :view
+  alias Requestbin.Bins.Request
 
   @doc """
   Shorten the `input` string if it excedes the `length`.
@@ -25,6 +26,20 @@ defmodule RequestbinWeb.RequestView do
       "…" <> String.slice(input, -length..-1)
     else
       input
+    end
+  end
+
+  @doc """
+  Build a short description of the request
+  """
+  @spec get_request_title(Request.t) :: String.t
+  def get_request_title(%Request{} = req) do
+    title = String.upcase(req.verb) <> " /" <> shorten_beginning(req.bin_id, 4) 
+
+    if String.length(req.query) > 0 do
+      title <> "?" <> req.query
+    else
+      title
     end
   end
 end
