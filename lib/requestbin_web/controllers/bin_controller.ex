f55ea7f6d54eafe1a72a031e87724ee054f73112
shadowjack <kostyap@hotmail.ru>
2019-02-19 22:18:03 +0300

    Save the list of created bins in cookies

--- lib/requestbin_web/controllers/bin_controller.ex
+++ lib/requestbin_web/controllers/bin_controller.ex
@@ -2,18 +2,26 @@ defmodule RequestbinWeb.BinController do
   use RequestbinWeb, :controller
 
   alias Requestbin.Bins
-  alias Requestbin.Bins.Bin
 
   def create(conn, _) do
     case Bins.create_bin() do
       {:ok, bin} ->
         conn
         |> put_flash(:info, "Bin #{bin.id} is created successfully.")
+        |> put_bin_to_session(bin.id)
         |> redirect(to: request_path(conn, :index, bin))
       {:error, %Ecto.Changeset{} = _changeset} ->
         conn
         |> put_flash(:error, "Something went wrong, please try again later")
         |> redirect(to: "/")
+    end
+  end
+
+  @spec put_bin_to_session(Conn.t, String.t) :: Conn.t
+  defp put_bin_to_session(conn, bin_id) do
+    case get_session(conn, :bins) do
+      nil -> put_session(conn, :bins, [bin_id])
+      bins -> put_session(conn, :bins, [bin_id | bins])
     end
   end
 end
--- lib/requestbin_web/controllers/homepage_controller.ex
+++ lib/requestbin_web/controllers/homepage_controller.ex
@@ -2,10 +2,18 @@ defmodule RequestbinWeb.HomepageController do
   use RequestbinWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", bins: get_bins_from_session(conn)
   end
 
   def not_found(conn, _params) do
     render conn, "404.html"
+  end
+
+  @spec get_bins_from_session(Conn.t) :: [String.t]
+  defp get_bins_from_session(conn) do
+    case get_session(conn, :bins) do
+      nil -> []
+      bins -> bins
+    end
   end
 end
