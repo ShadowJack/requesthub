c1053f709f5a5a6723bb9554864bf6415889ad19
shadowjack <kostyap@hotmail.ru>
2019-06-12 11:43:41 +0300

    Add tests for the RequestsLiveView

--- test/requestbin_web/live/requests_live_test.exs
+++ test/requestbin_web/live/requests_live_test.exs
@@ -1,5 +1,5 @@
 defmodule RequestbinWeb.RequestsLiveTest do
-  use RequestbinWeb.ChannelCase
+  use Requestbin.DataCase
 
   import Phoenix.LiveViewTest
 
@@ -7,22 +7,55 @@ defmodule RequestbinWeb.RequestsLiveTest do
   alias Requestbin.Test.Factory
 
   setup(ctx) do
-    bin = Factory.insert(:bin)
+    request = Factory.insert(:request)
 
-    [bin: bin]
+    query = from b in Requestbin.Bins.Bin, 
+      where: b.id == ^request.bin_id,
+      preload: [:requests]
+    bin = Requestbin.Repo.one(query)
+
+    [bin: bin, req_id: request.id]
   end
 
-  test "the list of requests is rendered", %{bin: bin} do
+  test "the list of requests is rendered", %{bin: bin, req_id: req_id} do
     {:ok, view, html} = mount(Endpoint, RequestsLive, session: %{bin: bin})
 
     assert html =~ "Latest requests"
+    assert html =~ req_id
   end
 
-  test "deleted request is removed from the view" do
+  test "it's possible to delete request", %{bin: bin, req_id: req_id} do
+    {:ok, view, _html} = mount(Endpoint, RequestsLive, session: %{bin: bin})
     
+    html = render_click(view, :delete_request, req_id)
+
+    assert (html =~ req_id) == false
+    assert Requestbin.Repo.get(Requestbin.Bins.Request, req_id) == nil
   end
 
-  test "new request is displayed on the view" do
-    
+  test "request is removed from the view once it's deleted by other user", %{bin: bin, req_id: req_id} do
+    {:ok, view, _html} = mount(Endpoint, RequestsLive, session: %{bin: bin})
+
+    msg = %{
+      event: RequestsLive.request_deleted_event(),
+      payload: %{request_id: req_id}
+    }
+    send(view.pid, msg)
+
+    assert (render(view) =~ req_id) == false
+  end
+
+  test "new request is displayed on the view", %{bin: bin, req_id: req_id} do
+    {:ok, view, _html} = mount(Endpoint, RequestsLive, session: %{bin: bin})
+
+    new_req = Factory.insert(:request, %{bin: bin})
+    msg = %{
+      event: RequestsLive.request_created_event(),
+      payload: %{request: new_req}
+    }
+    send(view.pid, msg)
+
+    assert render(view) =~ req_id
+    assert render(view) =~ new_req.id
   end
 end
